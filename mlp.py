c083992683d61cbef02b967923520e425d1bb176
Deathsnakeprime <doug.newquist@gmail.com>
2021-11-13 16:45:01 -0700

    Added Neuron Class

--- mlp.py
+++ mlp.py
@@ -1,6 +1,33 @@
+import math
+import random
+from typing import Callable, List
+
+
+def sigmoid(x):
+    return 1.0 / (1 + math.exp(-x))
+
+
+class Neuron:
+    bias: float = random.uniform(-1.0, 1.0)
+    weights: List[float]
+    sigmoid: Callable
+
+    def __init__(self, input_size, sigmoid) -> None:
+        self.weights = [random.uniform(-1.0, 1.0) for i in range(input_size)]
+        self.sigmoid = sigmoid
+
+    def evaluate(self, inputs):
+        total = self.bias
+
+        for input, weight in zip(inputs, self.weights):
+            total += input * weight
+
+        return self.sigmoid(total)
+
 
 def main():
-    pass
+    a = Neuron(5, sigmoid=sigmoid)
+    print(a.evaluate([1, 1, 1, 1, 1]))
 
 
 if __name__ == "__main__":
