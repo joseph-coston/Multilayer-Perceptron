7167837d3d99d2c5c2ed7ac21147ab71e5f93f38
Deathsnakeprime <doug.newquist@gmail.com>
2021-11-13 17:11:41 -0700

    Added Layer and Neural Net

--- mlp.py
+++ mlp.py
@@ -25,9 +25,45 @@ class Neuron:
         return self.sigmoid(total)
 
 
+class Layer:
+    neurons: List[Neuron]
+
+    def __init__(self, neurons, input_size=None, sigmoid=None) -> None:
+        if isinstance(neurons, list):
+            self.neurons = neurons
+        else:
+            self.neurons = [Neuron(input_size, sigmoid)
+                            for n in range(neurons)]
+
+    def evalute(self, inputs):
+        return [neuron.evaluate(inputs) for neuron in self.neurons]
+
+
+class NeuralNetwork:
+    layers: List[Layer]
+
+    def __init__(self, layers: list) -> None:
+        self.layers = layers
+
+    def evalute(self, inputs):
+        for layer in self.layers:
+            inputs = layer.evalute(inputs)
+
+        return inputs
+
+
 def main():
     a = Neuron(5, sigmoid=sigmoid)
     print(a.evaluate([1, 1, 1, 1, 1]))
+
+    layer = Layer([a])
+    print(layer.evalute([1, 1, 1, 1, 1]))
+
+    random_layer = Layer(3, 5, sigmoid)
+    print(random_layer.evalute([1, 1, 1, 1, 1]))
+
+    net = NeuralNetwork([random_layer, Layer(2, 3, sigmoid)])
+    print(net.evalute([1, 1, 1, 1, 1]))
 
 
 if __name__ == "__main__":
